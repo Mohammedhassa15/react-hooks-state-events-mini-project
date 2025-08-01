4eb4fc48b186dc022dc877e1e42407e414037f5f
mohammedhassa15 <mohammedhassan0041@gmail.com>
2025-08-01 16:23:44 +0300

    task completed

--- src/data.js
+++ src/data.js
@@ -1,32 +1,11 @@
-export const CATEGORIES = ["All", "Code", "Food", "Money", "Misc"];
+const CATEGORIES = ["All", "Code", "Food", "Money", "Misc"];
 
-export const TASKS = [
-  {
-    text: "Buy rice",
-    category: "Food",
-  },
-  {
-    text: "Save a tenner",
-    category: "Money",
-  },
-  {
-    text: "Build a todo app",
-    category: "Code",
-  },
-  {
-    text: "Build todo API",
-    category: "Code",
-  },
-  {
-    text: "Get an ISA",
-    category: "Money",
-  },
-  {
-    text: "Cook rice",
-    category: "Food",
-  },
-  {
-    text: "Tidy house",
-    category: "Misc",
-  },
+const TASKS = [
+  { text: "Buy rice", category: "Food" },
+  { text: "Save a tenner", category: "Money" },
+  { text: "Build a todo app", category: "Code" },
+  { text: "Bake a cake", category: "Food" },
+  { text: "Go to the bank", category: "Money" },
 ];
+
+export { CATEGORIES, TASKS };
